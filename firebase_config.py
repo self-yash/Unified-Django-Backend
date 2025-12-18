65b877bcff2164f24974b4f263efe5a8f3584a79
self-yash <yashmehta0005@gmail.com>
2025-12-18 22:32:15 +0530

    Path changes

--- firebase_config.py
+++ firebase_config.py
@@ -2,10 +2,10 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 cred = credentials.Certificate(
-    os.path.join(BASE_DIR, r"Unified-Django-Backend\firebase\secret_key.json")
+    os.path.join(BASE_DIR, r"firebase\secret_key.json")
 )
 
 if not firebase_admin._apps:
