7352f626b59c8f2ee277a2b23a4f4f484046ddcd
self-yash <yashmehta0005@gmail.com>
2025-12-19 21:08:02 +0530

    updated allowed hosts

--- UnifiedDjangoProject/settings.py
+++ UnifiedDjangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9ay3j)r*)&$cc!64-%ohd6fy599fao@v8qp(p&cifq5t1k7t^b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django.nexiotech.cloud']
 
 
 # Application definition
