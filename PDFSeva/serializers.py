a7690f62ec251789df48e99c864fe8d325762a62
self-yash <yashmehta0005@gmail.com>
2025-12-21 15:11:40 +0530

    added new fields

--- PDFSeva/serializers.py
+++ PDFSeva/serializers.py
@@ -6,3 +6,10 @@ class DeviceSerializer(serializers.Serializer):
     app_version = serializers.CharField(max_length=50)
     version_code = serializers.IntegerField()
     locale = serializers.CharField(max_length=10)
+    brand = serializers.CharField(max_length=100)
+    manufacturer = serializers.CharField(max_length=100)
+    model= serializers.CharField(max_length=100)
+    android_version = serializers.CharField(max_length=10)
+    sdk_version = serializers.IntegerField()
+    os_choices=[('Android', 'Android'), ('iOS', 'iOS')]
+    os= serializers.ChoiceField(choices=os_choices)
